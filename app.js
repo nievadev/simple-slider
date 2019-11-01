64baac5503962aef1f5706880ce51c0330d8b276
Martín Nieva <martinjafactor@gmail.com>
2019-11-01 00:48:16 -0300

    Works fine

--- app.js
+++ app.js
@@ -3,17 +3,36 @@ let index = 0;
 
 sliders[index].style.display = "block";
 
+document.body.addEventListener("keydown", event => {
+    if (event.key == "ArrowRight")
+    {
+        nextSlide(1);
+    }
+
+    else if (event.key == "ArrowLeft")
+    {
+        nextSlide(-1);
+    }
+});
+
 function nextSlide(val)
 {
-    if (index + val >= 0)
+    if (index + val >= 0 && index + val < sliders.length)
     {
         index += val;
     }
 
     sliders[index].style.display = "block";
 
+    if (sliders[index + 1])
+    {
+        sliders[index + 1].style.display = "none";
+    }
+    
     if (sliders[index - 1])
     {
         sliders[index - 1].style.display = "none";
     }
+
+    console.log(index);
 }
