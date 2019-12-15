51b740064f8994d158adc8b6c740eee3af3cae8d
Martín Nieva <martinjafactor@gmail.com>
2019-12-15 03:09:56 -0300

    Added more slides

--- code/wrapper.py
+++ code/wrapper.py
@@ -3,8 +3,6 @@ import curses
 def main(stdscr):
     height, width = stdscr.getmaxyx()
     cursor = stdscr.getyx()
-    
-    stdscr.addstr("{} : {}".format(cursor[0], cursor[1]))
 
     key = stdscr.getch()
 
